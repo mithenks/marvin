4b7c860fe8c5edd493f0b10dfc24003841551afd
Michele Mondelli <michele.mondelli@bonsaimeme.com>
2021-01-30 18:07:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Marvin 
+
+Packages and technologies used:
+ * [Micronaut Framework](https://micronaut.io/)
+ * [picocli](https://picocli.info/)
+ * [Docker](https://www.docker.com/)
+ * [GraalVM](https://www.graalvm.org/)
+ * [Native image](https://www.graalvm.org/reference-manual/native-image/)
+
+This project uses [bmeme/mn-dev](https://hub.docker.com/r/bmeme/mn-dev) image from [BMEME](https://www.bmeme.com).
+
+## Package the CLI application
+
+Run the docker container
+```bash
+$ docker-compose up -d
+```
+
+Package your application
+```bash
+$ ./cmvn clean compile test package
+```
+
+The application will be found in `app/target/maven`.
---
+++ app/src/main/java/marvin/MainCommand.java
@@ -0,0 +1,75 @@
+package marvin;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Command(name = "marvin", version="0.1", description = "Marvin Bot",
+        mixinStandardHelpOptions = true)
+public class MainCommand implements Runnable {
+
+    @Option(names = {"-v", "--verbose"}, description = "Be more verbose")
+    boolean verbose;
+
+    @Option(names = {"-p", "--project"}, description = "mn-dev, gatsby-dev, cjava")
+    private String project = null;
+
+    public static void main(String[] args) {
+        PicocliRunner.run(MainCommand.class, args);
+    }
+
+    public void run() {
+        System.out.println("ෆ Marvin is here");
+
+        if (verbose) {
+            System.out.println("Verbose option enabled");
+        }
+
+        if ( project != null ) {
+            if ( verbose ) {
+                System.out.println("Project to be initialized: " + project);
+            }
+            this.runCommand("git", "clone", "https://github.com/Mithenks/" + project, ".");
+            this.runCommand("rm", "-rf", ".git");
+        }
+    }
+
+    private void runCommand(String ...args) {
+
+        if (verbose ) {
+            System.out.println("Running command: " + String.join(" ", args));
+        }
+
+        ProcessBuilder processBuilder = new ProcessBuilder(args);
+        try {
+
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if ( verbose ) {
+                    System.out.println(line);
+                }
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                if ( verbose ) {
+                    System.out.println("Command executed correctly!");
+                }
+            } else {
+                System.out.println("Command execution returned an error code: " + exitVal);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
